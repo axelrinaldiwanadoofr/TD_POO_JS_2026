de64664e159cc4676cae1ced94da43f17d2debca
Axel Rinaldi <axel.rinaldi@wanadoo.fr>
2026-03-23 15:41:04 +0100

    Insertion du fichier Personne.js au moyen d'une balise script dans index.html

--- index.js
+++ index.js
@@ -1,62 +1,13 @@
-// Création d'un objet vide en JS
+// Création d'un objet de type Personne
+let p = new Personne() ;
 
-let a = new Object() ;
+// Initialise ses attributs
+p.nom = "MARTIN" ;
+p.prenom = "Paul" ;
+p.age = 20 ;
 
-// Ajout de l'attribut nom à l'objet référencé par a
-a.nom = "DUPOND" ;
-//Ajoute de l'attribut prénom
-a.prenom="Kenisha";
+// Affiche le contenu de la personne référencée p
+p.affiche() ;
 
-// ajout d'un age
-a.age = 54;
-
-// On affiche le nom de la personne
-function affichePersonne(p){
-    console.log( "nom: " + p.nom + " \nprenom: " + p.prenom + " \nage: " + p.age) ;
-}
-
-// appel de la fonction à partir de l'objet a
-affichePersonne(a);
-
-// Creation d'un objet vide avec une manière 
-
-let b = {} ;
-
-b.nom = "DURANT" ;
-b.prenom = "Pierre" ;
-b.age = 21 ;
-
-affichePersonne( b ) ;
-
-// Creation d'un objet en JSON
-
-let c = { nom: "MEYER", prenom: "Mathieu", age: 33 } ;
-
-affichePersonne( c ) ;
-
-let d={ nom:"YASAR",prenom:"Merve"};
-affichePersonne(d);
-
-// Ajout de la méthode affiche pour l'objet a
-
-a.affiche = function() 
-{
-    console.log( "nom: " + this.nom +
-         " \nprenom: " + this.prenom +
-          " \nage: " + this.age) ;
-}
-
-// Appel de la méthode affiche 
-console.log( "\n\nAppel de la méthode affiche à partir de l'objet a") ;
-a.affiche() ;
-
-// On cree l'atribut affiche pour b qui référence la même fonction que 
-// a.affiche
-b.affiche = a.affiche ;
-
-console.log( "\n\nAppel de la méthode affiche à partir de l'objet a") ;
-b.affiche() ;
-
-c.affiche() ;
 
 
