c8da9ce09266965cc10748b9f18d7126f2d62a61
Axel Rinaldi <axel.rinaldi@wanadoo.fr>
2026-05-04 16:37:38 +0200

    Ajout de la méthode updateModele et d'un listener de click sur le bouton OK

--- src/FrmPersonne.js
+++ src/FrmPersonne.js
@@ -6,6 +6,18 @@ export class FrmPersonne
     constructor( styleClass )
     {
         this.styleClass = styleClass ;
+
+        // Création d'un écouteur de click sur le bouton OK
+        // Recupère le formulaire
+        let frm = document.getElementsByClassName( this.styleClass )[0] ;
+
+        // Récupère les champs du formulaire
+        let btnOk = frm.getElementsByClassName( "BtnOK" )[0] ;
+        btnOk.addEventListener( "click", function()
+        {
+            alert( "click sur OK") ;
+        }) ;
+
     }
 
     updateForm( p )
@@ -21,6 +33,26 @@ export class FrmPersonne
         // Met à jour les champs
         inNom.value = p.nom ;
         inPrenom.value = p.prenom ;
-        inDateNaissance.value = p.dateNaissance.toLocaleDateString("us-US") ;
+        inDateNaissance.value = "" + 
+            p.dateNaissance.getFullYear() 
+            + "-" + p.dateNaissance.getMonth() 
+            + "-" + p.dateNaissance.getDate() ;
+    }
+
+    updateModele( p )
+    {
+        // Recupère le formulaire
+        let frm = document.getElementsByClassName( this.styleClass )[0] ;
+
+        // Récupère les champs du formulaire
+        let inNom = frm.getElementsByClassName( "nom" )[0] ;
+        let inPrenom = frm.getElementsByClassName( "prenom" )[0] ;
+        let inDateNaissance = frm.getElementsByClassName( "dateNaissance" )[0] ;
+
+        // Met à jour les propriétés de la personne à partir
+        // des champs du formulaire
+        p.nom = inNom.value ;
+        p.prenom = inPrenom.value ;
+        p.indDateNaissance = inDateNaissance.value ;
     }
 }
